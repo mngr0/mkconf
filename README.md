65b04cb6f33642730534c3ce42db442e02654cf9
mngr0 <ngrmarco@libero.it>
2019-04-24 11:05:49 -0400

    deriving from parport example

--- README.md
+++
@@ -1,2 +0,0 @@
-# mkconf
-my machinekit configuration for a delta 3d printer with high=60cm diameter=20cm
--- desktop/START.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#machinekit /home/machinekit/machinekit/configs/ARM.BeagleBone.CRAMPS/CRAMPS.ini DEBUG=5
-machinekit /home/machinekit/machinekit/configs/mkconf/CRAMPSDYZE.ini
