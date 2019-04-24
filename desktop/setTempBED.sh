c22c9476841f34dc2075f2b12a01fea7dc1b2da6
mngr0 <ngrmarco@libero.it>
2019-04-24 10:33:05 -0400

    initial commit parport_hal

--- desktop/setTempBED.sh
+++
@@ -1,4 +0,0 @@
-echo "inserire temp bed"
-read TEMP
-#echo $TEMP
-halcmd sets bed.temp.set $TEMP
--- desktop/setTempE0.sh
+++
@@ -1,4 +0,0 @@
-echo "inserire temp estrusore"
-read TEMP
-#echo $TEMP
-halcmd sets e0.temp.set $TEMP
