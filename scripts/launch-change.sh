e15558a9a46c7983c57189ba383c02f80f9eba1e
mngr0 <ngrmarco@libero.it>
2019-05-24 11:38:00 -0400

    added desktop

---
+++ scripts/launch-change.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source ~/machinekit/scripts/rip-environment
+echo "Environment set up for running Machinekit"
+bash -c /home/machinekit/Documents/gh/mkconf/scripts/changeH.py /home/machinekit/Documents/gh/mkconf/adv_stepper_inch.ini
+
